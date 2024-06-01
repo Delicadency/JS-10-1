bd9dd6c00c27158608d4059080d665c41a71d1d5
Agata Kamińska <a.fluffypie@gmail.com>
2024-06-01 19:35:29 +0200

    added optional chaining for urlImg, created error messages

--- script.js
+++ script.js
@@ -11,12 +11,27 @@ clickedButton.addEventListener("click", () => {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const urlImg = data[0].url;
-      const img = document.createElement("img");
-      img.src = urlImg;
-      img.setAttribute("data-test", "img-kitty");
-      document.body.appendChild(img);
-      return img;
+      const urlImg = data?.[0]?.url;
+      if (urlImg) {
+        const img = document.createElement("img");
+        img.src = urlImg;
+        img.setAttribute("data-test", "img-kitty");
+        document.body.appendChild(img);
+        return img;
+      } else {
+        const errorInfo = document.createElement("p");
+        errorInfo.innerText =
+          "Nie można obecnie wyświetlić obrazu. Spróbuj ponownie później.";
+        document.body.appendChild(errorInfo);
+        return errorInfo;
+      }
     })
-    .catch((error) => console.error(error));
+    .catch((error) => {
+      const errorInfo = document.createElement("p");
+      errorInfo.innerText =
+        "Nie można obecnie wyświetlić obrazu. Spróbuj ponownie później.";
+      document.body.appendChild(errorInfo);
+      console.error(error);
+      return errorInfo;
+    });
 });
